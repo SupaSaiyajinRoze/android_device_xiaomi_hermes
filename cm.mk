bda56b6cdfe668598b67261a5fb1cf5a04c640cd
Dinolek <dinolek@o2.pl>
2017-02-25 20:06:31 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hermes
-PRODUCT_NAME := cm_hermes
-PRODUCT_BRAND := XiaoMi
+PRODUCT_NAME := lineage_hermes
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 2
-PRODUCT_MANUFACTURER := XiaoMi
+PRODUCT_MANUFACTURER := Xiaomi
